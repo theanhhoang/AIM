e109e659e09432240fe3915de6192b9e92396027
Eugene Lin <eugenezlin@gmail.com>
2021-07-06 23:27:46 -0700

    PBS weighted sum

---
+++ PBS-SIPP-IMP2/PTNode.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <set>
+#include <tuple>
+#include <vector>
+#include "Common.h"
+#include "Instance.h"
+
+const double EPSILON = 0.000001;
+
+class PTNode{
+public:
+	vector<Path> plan; //plan is vector of paths	size of vector = size of vehicles
+	std::map<int, std::set<int> > priority; //map of index with set of lower prio
+	double cost;
+	Instance& instance;
+	PTNode();
+	PTNode(vector<Path>, std::map<int, std::set<int> >, Instance&);
+	void writeToFile(Instance&, const string&);	
+	void calculateCost();
+	void topologicalSortUtil(int, bool*, std::list<int>&);
+	std::list<int> topologicalSort();
+	std::list<int> topologicalSort(int);
+	std::tuple<int, int, int> getFirstCollision(Instance&);
+	//void getRT(ReservationTable&, int);
+	void getRTP(std::set<int>&, int);
+	void getRTFromP(Instance&, ReservationTable&, std::set<int>);
+	void getRTFromP(Instance&, ReservationTable&, std::set<int>, int,std::map<int, std::map<int,std::vector<int> > >&);
+};
+
